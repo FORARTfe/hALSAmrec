47bb98351782af869db0abe411e2b20c70ce3e1e
Marco Ravich <forartfe@gmail.com>
2026-06-18 19:59:18 +0200

    Change default ACTION from 'install' to 'uninstall'

--- test/vutest2.sh
+++ test/vutest2.sh
@@ -19,7 +19,7 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Defaults
-ACTION="install"
+ACTION="uninstall"
 
 # Paths - FIXED: proper path concatenation with slashes
 OPENWRT_ROOT="${OPENWRT_ROOT:-}"
